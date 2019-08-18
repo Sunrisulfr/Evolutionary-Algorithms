8849fb01aec858050d03af05227afa64b3e0da13
envy0997 <1402880997@qq.com>
2019-08-18 16:12:31 +0800

    add GA matlab code

---
+++ GA/selection.m
@@ -0,0 +1,16 @@
+function [newpop]=selection(pop,fitvalue) 
+totalfit=sum(fitvalue);                   %求适应值之和
+fitvalue=fitvalue/totalfit;                %单个个体被选择的概率
+fitvalue=cumsum(fitvalue);            %如 fitvalue=[1 2 3 4]，则 cumsum(fitvalue)=[1 3 6 10]，不明白为什么要累加 
+[px,py]=size(pop);                       %20*10
+ms=sort(rand(px,1));                   %从小到大排列
+fitin=1;
+newin=1;
+while newin<=px                          %选出20个新个体，有重复情况，和上面介绍的方法不太一样
+        if(ms(newin))<fitvalue(fitin)
+                newpop(newin,:)=pop(fitin,:);
+                newin=newin+1;
+        else
+                fitin=fitin+1;
+        end
+end
